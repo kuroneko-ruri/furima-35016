ad2fe601a7fd82287c0a6e7a7eedb8e693b5dda2
kuroneko-ruri <kuroneko4126@icloud.com>
2021-04-02 12:40:52 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   #before_action :authenticate_user!
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -9,4 +10,11 @@ class ApplicationController < ActionController::Base
     )
   end
 
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'kuroneko' && password == '4126'
+    end
+  end
 end
